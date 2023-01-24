fd99709bda2a089c839cc7ccb0c2bc846fdb8e8e
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2023-01-24 23:34:06 +0530

    created te text components and fixed responsivness

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import background from '../../assets/background.webp'
 import '../../components/Main/style/style.css'
+import star from "../../assets/star.svg"
 
 function index() {
   return (
     <>
-        <img src={background} alt='background' className='background-image'/>
+      <img src={background} alt='background' className='background-image' />
+      <div className="header-box">
+        <div className="header-tagline-box">
+          <img src={star} alt="star" />
+          <p className='header-tagline'>VIT Chennai's Annual Sports and Cultural Fest</p>
+          <img src={star} alt="star" />
+        </div>
+        <p className='header-text'>Vibrance is Back</p>
+      </div>
+
+      <div className="countdown-box">
+        <div className="countdown-tagline">Get ready to be Engaged, Enthralled, Entertained in</div>
+        <div className="counter">
+          <div className="counter-day">15:</div>
+          <div className="counter-minute">32:</div>
+          <div className="counter-seconds">06</div>
+        </div>
+      </div>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/Gilroy-BoldItalic.ttf';
+import './fonts/Cirka-Bold.otf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
