4f95ed4182f89e3fb8ef0ec1e9935b19ec59068b
Mohammad Shaad Shaikh <mohammadshaadshaikh@Shaad-MacBook-Pro.local>
2023-01-28 12:42:36 +0530

    Added gallery part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Index from './components/Navbar';
 import Main from './components/Main';
+import Gallery from './components/Gallery'
 
 function App() {
   return (
     <>
       <Index />
       <Main />
+      <Gallery />
     </>
   );
 }
