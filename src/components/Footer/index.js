c4dffc8207736d4e18ed67b4d5c04f9c053cb7c5
Mohammad Shaad Shaikh <mohammadshaadshaikh@Shaad-MacBook-Pro.local>
2023-01-29 18:25:08 +0530

    Modified Contact Us

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import '../../components/Footer/styles/style.css'
 import "animate.css/animate.min.css";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
-
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function index() {
     return (
@@ -11,17 +12,11 @@ function index() {
             <AnimationOnScroll animateIn="animate__fadeIn">
                 <div className='footer mt-20 sm:mt-10 w-full flex items-center justify-center'>
 
-                    <div className='footer-wrapper w-3/4 rounded-xl p-8 flex items-center justify-center flex-col'>
-                        <h1 className="text-lg font-bold mb-4">CONTACT US</h1>
-                        <form className='w-full'>
-                            <input type="text" className="border rounded-md p-2 mb-4 w-full" placeholder='Name' />
-                            <input type="email" className="border rounded p-2 mb-4 w-full" placeholder='Email' />
-                            <textarea className="border rounded p-2 mb-4 w-full" rows={4} placeholder='Message' />
-                            <button className="send-button text-white py-2 px-6 rounded-lg">
-                                Send
-                            </button>
-                        </form>
-
+                    <div className='footer-wrapper w-3/4 rounded-xl p-8 flex items-center justify-between m-10'>
+                        <h1 className="text-lg font-medium">CONTACT US</h1>
+                        <div className='instagram-icon text-black'>
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </div>
                     </div>
 
                 </div>
