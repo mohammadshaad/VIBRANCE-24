3f915e1f0834994d36ed9c08f063f0018a85d87e
Mohammad Shaad Shaikh <mohammadshaadshaikh@Shaad-MacBook-Pro.local>
2023-01-29 12:35:06 +0530

    Major and minor modifications

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import '../../components/Gallery/styles/style.css'
-import arrow from '../../assets/Nav Arrow.webp'
+// import arrow from '../../assets/Nav Arrow.webp'
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+
 
 
 function index() {
     return (
         <>
-            <div className='slider-container flex items-center justify-center'>
-                <div className='slider flex items-center justify-center flex-col'>
-                    <div className='slider-row flex items-center justify-center'>
+            <AnimationOnScroll animateIn="animate__fadeIn">
+
+                <div className='slider-container flex items-center justify-center'>
+                    <div className='slider flex items-center justify-center flex-col'>
+                        <div className='slider-row flex items-center justify-center'>
+                        </div>
+                        {/* <button className='viewmore flex items-center justify-center gap-4'>Click Here <img src={arrow} alt='arrow'/></button> */}
                     </div>
-                    {/* <button className='viewmore flex items-center justify-center gap-4'>Click Here <img src={arrow} alt='arrow'/></button> */}
                 </div>
-            </div>
+            </AnimationOnScroll>
+
         </>
     )
 }
